7981b856049ee0b222ff50452429030ec3ad7287
Mounir Dellagi <mounir.dellagi@wallapop.com>
2020-03-13 12:39:00 +0100

    Adding Package Swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "PINRemoteImage",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "PINRemoteImage", targets: ["PINRemoteImage"])
+    ],
+    targets: [
+        .target(
+            name: "PINRemoteImage",
+            path: "PINRemoteImage",
+            publicHeadersPath: nil
+        )
+    ]
+)
