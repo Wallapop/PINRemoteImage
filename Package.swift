a3dc3aa4512410213091fe4b5118f46aa9f0ad62
Mounir Dellagi <mounir.dellagi@wallapop.com>
2020-03-13 13:30:11 +0100

    Just using public path in package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .target(
             name: "PINRemoteImage",
-            path: "PINRemoteImage",
+            path: "PINRemoteImage/Public",
             publicHeadersPath: nil
         )
     ]
