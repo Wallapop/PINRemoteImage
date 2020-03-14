e4794f8376071e7d77e38b200088b8f751016782
Mounir Dellagi <mounir.dellagi@wallapop.com>
2020-03-14 12:06:40 +0100

    Correcting path

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .target(
             name: "PINRemoteImage",
-            path: "PINRemoteImage/Public",
+            path: "PINRemoteImage/Source",
             publicHeadersPath: nil
         )
     ]
